d5e0e49e71bdb9822960421ce7cc45e97b881f46
Daniyar Otegen <daniyarotegen@gmail.com>
2023-02-17 14:28:51 +0600

    Initial commit - added 'details' field to the model, modified views and templates to display the new field

---
+++ todo/views.py
@@ -0,0 +1,24 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.shortcuts import render, redirect
+from todo.models import Task
+
+
+def index_view(request: WSGIRequest):
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context=context)
+
+
+def add_view(request: WSGIRequest):
+    if request.method == 'GET':
+        return render(request, 'task_create.html', {'task': Task()})
+    task_data = {
+        'description': request.POST.get('description'),
+        'details': request.POST.get('details'),
+        'status': request.POST.get('status'),
+        'completion_date': request.POST.get('completion_date')
+    }
+    Task.objects.create(**task_data)
+    return redirect('/')
